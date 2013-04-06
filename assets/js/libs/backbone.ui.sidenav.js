79bae4a19b3ee09a41ac7b026862a55554977ec7
Lyndel Thomas <lyndelt@gmail.com>
2013-04-06 01:19:16 -0700

    first commit

---
+++ assets/js/libs/backbone.ui.sidenav.js
@@ -0,0 +1,13 @@
+if( _.isUndefined( Backbone.UI ) ) Backbone.UI = {};
+
+Backbone.UI.Sidenav = Backbone.View.extend({
+	
+	events: {
+		"click #nav-collapse": "toggle"
+	},
+
+	toggle: function() {
+		$(this.el).toggleClass('shift');
+	}
+
+});
