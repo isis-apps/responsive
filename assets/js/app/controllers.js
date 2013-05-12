6690c8e3d458a9de620ede736df585303e9a1e2c
Lyndel Thomas <lyndelt@gmail.com>
2013-05-11 19:45:23 -0700

    adding index route

--- assets/js/app/controllers.js
+++ assets/js/app/controllers.js
@@ -1,23 +1,33 @@
 APP.Routers.Default = APP.Router.extend({
 	initialize: function(){
 		// instantiate view
+		_.bindAll(this, "index");
+	},
+	routes: {
+		"": "index"	
+	},
+	index: function(){
+		
 		var sidenavView = new APP.Views.Sidenav({
 			el : 'body > .ui-sidenav'
 		});
 		if( !this.state.mobile ){ 
 				$(window).bind("resize", _.bind(sidenavView.resize, sidenavView));
 		}
-		var sliderView = new Backbone.UI.Slider({
-			el : "#slider", 
+		
+		var slideshowView = new Backbone.UI.Slideshow({
+			el : "#slideshow", 
 			collection : new Backbone.Collection(slides),
-			url : "assets/html/slider.html"
+			url : "assets/html/slides.html"
 		});
 		
 		var collapsibleView = new Backbone.UI.Collapsible({
 			el : '.ui-collapsible'
 		})
 		
-		sliderView.render();
+		slideshowView.render();
+
+	
 	}
 });
 
